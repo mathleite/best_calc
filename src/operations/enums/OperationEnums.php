25f31eb07acfc05c265a91979ec82aedf8fec47a
joao1020 <90358370+joao1020@users.noreply.github.com>
2021-10-31 23:08:54 -0300

    Update Class Enums

---
+++ src/operations/enums/OperationEnums.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\operations\enums;
+
+class OperationEnums
+{
+    public const DIVISION = 'division'
+    public const MULTIPLICACAO = "multiplicacao"
+    public const SUBTRACTION = "subtraction"
+    public const SUM = "sum"
+}
